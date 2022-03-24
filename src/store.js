628bbe21488a6a8544bd9886b1e9049c7297d42b
denis <permwed@gmail.com>
2022-03-24 15:14:30 +0500

    add redux

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware, combineReducers, compose } from "redux";
+import apiReducer from "./modules/api/reducer";
+
+const reducers = combineReducers({
+  api: apiReducer,
+});
+
+const store = createStore(reducers);
+export default store;
