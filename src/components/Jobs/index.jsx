068035113b0994eff30b23d924e6ba0650fa2d3f
denis <permwed@gmail.com>
2022-03-25 10:01:34 +0500

    add material ui

--- src/components/Jobs/index.jsx
+++ src/components/Jobs/index.jsx
@@ -1,28 +1,18 @@
 // Libraries
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { apiActions } from "../../modules/api/actions";
 // Modules
-import api from "../../modules/api/api";
 import { JOBS } from "../../modules/api/endpoints";
+// Hooks
+import useFetch from "../../hooks/useFetch";
 
 export default function Jobs() {
-  const state = useSelector((state) => state.api[JOBS]);
-  const dispatch = useDispatch();
+  const { response, performFetch } = useFetch(JOBS);
 
-  useEffect(async () => {
-    try {
-      dispatch(apiActions.fetch(JOBS));
+  useEffect(() => {
+    performFetch();
+  }, [performFetch]);
 
-      const data = await api.fetch(JOBS);
-
-      dispatch(apiActions.fetchSuccess(JOBS, data));
-    } catch (error) {
-      dispatch(apiActions.fetchFailure(JOBS, error));
-    }
-  }, []);
-
-  console.log(state);
+  console.log(response);
 
   return <div>Jobs Component</div>;
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import CssBaseline from "@mui/material/CssBaseline";
 // App
 import App from "./App";
 import store from "./store";
@@ -9,6 +10,7 @@ import store from "./store";
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <CssBaseline />
       <App />
     </Provider>
   </React.StrictMode>,
